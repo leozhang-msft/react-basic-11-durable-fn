83195a768e6553130eb8c718cd4f562f6140c8e1
Leo Zhang <t-leozhang@microsoft.com>
2023-08-10 10:52:23 -0700

    add comments

--- src/App.js
+++ src/App.js
@@ -7,18 +7,22 @@ function App() {
     (async function () {
       const API_PATH = '/api/orchestrators/HelloOrchestrator'
 
+      // get the status query from the orchestrator
       let { statusQueryGetUri } = await( await fetch(API_PATH)).json();
+      // remove the host name from the status query
       statusQueryGetUri = statusQueryGetUri.substring(statusQueryGetUri.indexOf('/runtime/'));
       console.log(statusQueryGetUri);
 
       let statusJson = await (await fetch(statusQueryGetUri)).json();
       console.log(statusJson);
 
+      // wait for the function to complete
       while(statusJson.runtimeStatus === "Running" || statusJson.runtimeStatus === "Pending") {
         statusJson = await (await fetch(statusQueryGetUri)).json();
         console.log(statusJson);
       }
 
+      // display the output
       console.log(typeof(statusJson.output))
       setData(statusJson.output.join(', '));
     })();
