1cbefd87c272e3fb08ee25469b97411bb1f35fa7
Leo Zhang <t-leozhang@microsoft.com>
2023-08-10 14:10:39 -0700

    get react router working

--- src/App.js
+++ src/App.js
@@ -1,58 +1,84 @@
-import React, { useState, useEffect } from 'react';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { BrowserRouter, Routes, Route, Link, Outlet } from "react-router-dom";
 
 function Home() {
-  const [data, setData] = useState('Loading msg from API...');
+  const [data, setData] = useState("Loading msg from API...");
 
   useEffect(() => {
     (async function () {
-      const API_PATH = '/api/orchestrators/HelloOrchestrator'
+      const API_PATH = "/api/orchestrators/HelloOrchestrator";
 
       // get the status query from the orchestrator
-      let { statusQueryGetUri } = await( await fetch(API_PATH)).json();
+      let { statusQueryGetUri } = await (await fetch(API_PATH)).json();
       // remove the host name from the status query
-      statusQueryGetUri = statusQueryGetUri.substring(statusQueryGetUri.indexOf('/runtime/'));
+      statusQueryGetUri = statusQueryGetUri.substring(
+        statusQueryGetUri.indexOf("/runtime/")
+      );
       console.log(statusQueryGetUri);
 
       let statusJson = await (await fetch(statusQueryGetUri)).json();
       console.log(statusJson);
 
       // wait for the function to complete
-      while(statusJson.runtimeStatus === "Running" || statusJson.runtimeStatus === "Pending") {
+      while (
+        statusJson.runtimeStatus === "Running" ||
+        statusJson.runtimeStatus === "Pending"
+      ) {
         // sleep 1s
-        await new Promise(resolve => setTimeout(resolve, 1000));
+        await new Promise((resolve) => setTimeout(resolve, 1000));
 
         statusJson = await (await fetch(statusQueryGetUri)).json();
         console.log(statusJson);
       }
 
       // display the output
-      console.log(typeof(statusJson.output))
-      setData(statusJson.output.join(', '));
+      console.log(typeof statusJson.output);
+      setData(statusJson.output.join(", "));
     })();
   }, []);
 
-  return <div>{data}</div>;
+  return (
+    <div>
+      <h1>TEST</h1>
+      <div>{data}</div>
+    </div>
+  );
 }
 
 function About() {
-  return <div>About</div>;
+  return <div>About, yo why you no work</div>;
+}
+
+function Navbar() {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Outlet />
+    </div>
+  );
 }
 
 function App() {
   return (
     <BrowserRouter>
-
-      <div>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-      </div>
-
-      <Route path="/" exact component={Home} />
-      <Route path="/about" component={About} />
-
+      <Routes>
+        <Route path="/" element={<Navbar />}>
+          <Route index element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Route>
+      </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
 export default App;
