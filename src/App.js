c2b04627bd8073c40d17371ab723f20a4fe57ce9
Leo Zhang <t-leozhang@microsoft.com>
2023-08-10 10:18:59 -0700

    update api

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  const [data, setData] = useState('Loading msg from API...');
+
+  useEffect(() => {
+    (async function () {
+      const API_PATH = '/api/orchestrators/HelloOrchestrator'
+
+      let { statusQueryGetUri } = await( await fetch(API_PATH)).json();
+      // nevermind, this doesn't work, directs to the react app.
+      // statusQueryGetUri = statusQueryGetUri.replace('https://leozhang-js-durable-func-app.azurewebsites.net', '');
+      console.log(statusQueryGetUri);
+
+      const status = (await fetch(statusQueryGetUri));
+      const body = status.body
+      console.log(body);
+
+      setData(statusQueryGetUri);
+    })();
+  }, []);
+
+  return <div>{data}</div>;
 }
 
 export default App;
